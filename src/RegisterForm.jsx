59dee5b88d471f9d683abe3c8c2ca6b01a126517
Path3010 <rpathpatel@gmail.com>
2025-08-05 22:07:28 +0530

    Formating

--- src/RegisterForm.jsx
+++ src/RegisterForm.jsx
@@ -52,56 +52,59 @@ function RegisterForm() {
     };
 
     return (
-        <div>
+        <>
             <h2>Register</h2>
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Name: </label>
-                    <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter Name" />
-                </div>
-                <div>
-                    <label>Email: </label>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email" />
-                </div>
-                <div>
-                    <label>Mobile: </label>
-                    <input value={mobile} onChange={(e) => setMobile(e.target.value)} placeholder="Enter Mobile No" />
-                </div>
-                <div>
-                    <label>Semester: </label>
-                    <select value={semester} onChange={(e) => setSemester(e.target.value)}>
-                        <option value="">Select</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                    </select>
-                </div>
-                <div>
-                    <label>Gender: </label>
-                    <input type="radio" name="gender" value="Male" onChange={(e) => setGender(e.target.value)} /> Male
-                    <input type="radio" name="gender" value="Female" onChange={(e) => setGender(e.target.value)} /> Female
-                </div>
-                <div>
-                    <label>Message: </label><br />
-                    <textarea value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Enter Message" />
-                </div>
-                <div>
-                    <label>Password: </label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" />
-                </div>
-                <div>
-                    <label>Confirm Password: </label>
-                    <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Enter Confirm Password" />
-                </div>
-                <div>
-                    <label>
-                        <input type="checkbox" checked={agree} onChange={(e) => setAgree(e.target.checked)} />
-                        I Agree Terms & Conditions
-                    </label>
-                </div>
+                <label>Name: </label>
+                <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter Name" /><br /><br />
+
+                <label>Email: </label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email" /><br /><br />
+
+                <label>Mobile: </label>
+                <input value={mobile} onChange={(e) => setMobile(e.target.value)} placeholder="Enter Mobile No" /><br /><br />
+
+                <label>Semester: </label>
+                <select value={semester} onChange={(e) => setSemester(e.target.value)}>
+                    <option value="">Select</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select><br /><br />
+
+                <label>Gender: </label>
+                <input
+                    type="radio"
+                    name="gender"
+                    value="Male"
+                    checked={gender === 'Male'}
+                    onChange={(e) => setGender(e.target.value)}
+                /> Male
+                <input
+                    type="radio"
+                    name="gender"
+                    value="Female"
+                    checked={gender === 'Female'}
+                    onChange={(e) => setGender(e.target.value)}
+                /> Female<br /><br />
+
+                <label>Message: </label><br />
+                <textarea value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Enter Message" /><br /><br />
+
+                <label>Password: </label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" /><br /><br />
+
+                <label>Confirm Password: </label>
+                <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Enter Confirm Password" /><br /><br />
+
+                <label>
+                    <input type="checkbox" checked={agree} onChange={(e) => setAgree(e.target.checked)} />
+                    I Agree Terms & Conditions
+                </label><br /><br />
+
                 <button type="submit">Submit</button>
             </form>
-        </div>
+        </>
     );
 }
 
